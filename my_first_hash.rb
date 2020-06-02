79292af42c298c6d686373b4e08b18a4e2251fe8
Alain Saint-Dic <alains214@gmail.com>
2020-06-02 19:10:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,6 @@
-def my_hash
+def my_hash(foods)
+  foods = {"breakfast" =>  "eggs"}
+end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
